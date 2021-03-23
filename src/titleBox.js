7fd1bd9b5fc6b828e0b9ad14791aa851f0cc718d
nosharp <nosharp1@gmail.com>
2021-03-23 18:48:42 +0000

    feat: Add title box element for titles

---
+++ src/titleBox.js
@@ -0,0 +1,25 @@
+import './titleBox.css'
+import React from "react"
+import PropTypes from "prop-types"
+/**
+ * Represents a question and answer box.
+ */
+class TitleBox extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <div className="titleBox">
+            <h1 className="titleBoxText">{this.props.title}</h1>
+        </div>
+    }
+
+}
+
+TitleBox.propTypes = {
+    title: PropTypes.string
+}
+
+export default TitleBox;
