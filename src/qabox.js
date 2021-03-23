f2d4a0ab82212649d54ab9202bc132702ecd8634
nosharp <nosharp1@gmail.com>
2021-03-23 16:16:09 +0000

    fix: Button groups on mobile appearing outside of container

--- src/qabox.js
+++ src/qabox.js
@@ -11,10 +11,10 @@ class QAbox extends React.Component{
         return <div className="qabox">
             <h1 className="qaboxText">{this.props.question}</h1>
             <div className="btn-group answerbox" role="group" data-toggle="buttons">
-                 <label class="btn btn-secondary" for={"true." + this.props.number}>
+                 <label class="btn btn-secondary answerbox-answer" for={"true." + this.props.number}>
                     <input class="btn-check" type="radio" name="options" id={"true." + this.props.number} autocomplete="off"/>True
                 </label>
-                <label class="btn btn-secondary" for={"false." + this.props.number}>
+                <label class="btn btn-secondary answerbox-answer" for={"false." + this.props.number}>
                     <input class="btn-check" type="radio" name="options" id={"false." + this.props.number} autocomplete="off"/>False
                 </label>
 
