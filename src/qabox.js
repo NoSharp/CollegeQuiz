97066a4166d023b8c3cfe1af3974368c64edcf2a
nosharp <nosharp1@gmail.com>
2021-03-23 18:48:02 +0000

    fix: Mutliple button groups not being independant

--- src/qabox.js
+++ src/qabox.js
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import './qabox.css'
 import React from "react"
 
@@ -6,27 +5,53 @@ import React from "react"
  * Represents a question and answer box.
  */
 class QAbox extends React.Component{
-     
+
+    constructor(props){
+        super(props);
+        this.state = {}
+        this.setState({
+            question: props.question,
+            number: props.number,
+            selected: true
+        })
+        this.setSelectedAnswer(false);
+    }
+
     render(){
+        const radioClass = "btn btn-primary answerbox-answer" 
         return <div className="qabox">
             <h1 className="qaboxText">{this.props.question}</h1>
-            <div className="btn-group answerbox" role="group" data-toggle="buttons">
-                 <label class="btn btn-secondary answerbox-answer" for={"true." + this.props.number}>
-                    <input class="btn-check" type="radio" name="options" id={"true." + this.props.number} autocomplete="off"/>True
+            <div className="btn-group answerbox" data-toggle="buttons">
+                 <label className={ this.isAnswerSelected(true) ? "selected-answer " + radioClass : radioClass} htmlFor={"true." + this.props.number}>
+                    <input className="btn-check" type="radio" name={"true." + this.props.number} id={"true." + this.props.number} autoComplete="off" onClick={
+                        ()=>{
+                           this.setSelectedAnswer(true);
+                        }
+                    }/>True
                 </label>
-                <label class="btn btn-secondary answerbox-answer" for={"false." + this.props.number}>
-                    <input class="btn-check" type="radio" name="options" id={"false." + this.props.number} autocomplete="off"/>False
+                <label className={ this.isAnswerSelected(false) ? "selected-answer " + radioClass : radioClass} htmlFor={"false." + this.props.number}>
+                    <input className="btn-check" type="radio" name={"false." + this.props.number} id={"false." + this.props.number} autoComplete="off" onClick={
+                        ()=>{
+                           this.setSelectedAnswer(false);
+                        }
+                    }/>False
                 </label>
-
-            </div>
+            </div> 
         </div>
     }
 
+    setSelectedAnswer(answer){
+        this.setState({
+            question: this.props.question,
+            number: this.props.number,
+            selected: answer
+        });
+    }
+
+    isAnswerSelected(answer){
+        return this.state.selected !== undefined && this.state.selected === answer;
+    }
 }
 
-QAbox.propTypes = {
-    question: PropTypes.string,
-    number: PropTypes.number
-}
 
 export default QAbox;
