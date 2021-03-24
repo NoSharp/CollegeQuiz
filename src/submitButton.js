ee4d293a2a30b4e4f42cf3c5ac8f81e7a90dcc9a
nosharp <nosharp1@gmail.com>
2021-03-24 20:35:27 +0000

    feat: Add score handling to submit button

--- src/submitButton.js
+++ src/submitButton.js
@@ -1,16 +1,33 @@
 import './submitButton.css'
 import React from "react"
-import PropTypes from "prop-types"
+import AnswerModal from "./answerModal";
 /**
  * Represents a question and answer box.
  */
 class submitButton extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            "answers":{},
+            "selected": false
+        };
+
+    }
+
     render(){
+        let modal;
+        if(this.state.selected === true){
+            modal = <AnswerModal column1={this.state.answers1} column2={this.state.answers2} percentage={this.state.percentage} correctAmount={this.state.correctAmount}></AnswerModal>
+        }else{
+            modal = <></>
+        }
+
         return <div className="submitButton">
             <button type= "button" className="btn btn-success" onClick={
                 ()=>{
-                    const answers = this.getAnswersFromContainer();
+                    if(this.state.selected) return;
+                    const answers = this.getUserAnswersFromContainer();
                     const gameUUID = this.getGameUUID();
                     let correctAnswers = {};
                     fetch(`http://localhost:8080/api/v1/GetAnswer/${gameUUID}`,{
@@ -20,24 +37,52 @@ class submitButton extends React.Component{
                         .then(res=>res.json())
                         .then((res)=>{
                             const answerList = res.answerList;
-                            for(const answerKey in answerList){
-                                correctAnswers[answerKey] = answers[answerKey] == answerList[answerKey];
-                            }
-                            console.log(correctAnswers);
+                            
+                            const answers = this.getDisplayedTextFromAnswers(answerList);
+                            let succ = answers[2];
+                            console.log(succ);
+                            console.log(Math.round((succ/10)*100, 0));
+                            this.setState({
+                                "answers1": answers[0],
+                                "answers2": answers[1],
+                                "selected": true,
+                                "percentage":(succ/10)*100,
+                                "correctAmount": succ
+                            })
                         })
                         .catch(err=> console.log(` ERROR: ${err}`))
                 }
-            }>Submit</button>
+            } data-bs-toggle="modal" data-bs-target="#answerBoxModal">Submit</button>
+            {modal}
         </div>
     }
 
-    getAnswersFromContainer(){
+    getDisplayedTextFromAnswers(answers){
+        const answerList = this.getUserAnswersFromContainer();
+        let column1 = "";
+        let column2 = "";
+        let correct = 0;
+        for(let i = 0; i < 5; i++){
+            const isCorrect = answers[i] == answerList[i][0];
+            if(isCorrect) correct++;
+            column1 += `Question ${i+1}: ${isCorrect ? "✅" : "❌"},`;
+        }
+        for(let i = 5; i < 10; i++){
+            const isCorrect = answers[i] == answerList[i][0];
+            if(isCorrect) correct++;
+            column2 += `Question ${i+1}: ${isCorrect ? "✅" : "❌"},`;
+        }
+        return [column1, column2, correct]
+    }
+
+    getUserAnswersFromContainer(){
         const selectedElements = this.getAllSelectedElements();
+        const questions = this.getAllQuestions();
         let answers = {};
         for(const element in selectedElements){
             if(isNaN(element)) continue;
             const parsedAnswerAndId = this.parseAnswerFromIdOfInput(selectedElements[element].id);
-            answers[parsedAnswerAndId.id] = parsedAnswerAndId.answer;
+            answers[parsedAnswerAndId.id] = [parsedAnswerAndId.answer,questions[parsedAnswerAndId.id].innerText]
         }
 
         return answers;
@@ -58,6 +103,10 @@ class submitButton extends React.Component{
 
     getAllSelectedElements(){
         return document.querySelectorAll(".selected-answer>input");
+    }
+
+    getAllQuestions(){
+        return document.querySelectorAll(".qaboxText");
     }
 
     getRadioFromElement(element){
