a1fbcd318ca543d0932abcf04435679433ffabdb
nosharp <nosharp1@gmail.com>
2021-03-24 20:34:40 +0000

    fix: Fix background colour on mobile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
    
     <div className="App">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
-      <QABoxContainer ></QABoxContainer>
+      <QABoxContainer ></QABoxContainer> 
     </div>
   );
 }
